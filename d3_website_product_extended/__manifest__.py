179e858baec0b2e4927b6a84a1e1b7209cd4a259
Juan Arcos <juanparmer@gmail.com>
2025-03-24 09:23:37 -0500

    development [ADD] d3_website_product_extended: add module

---
+++ d3_website_product_extended/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'D3 Website Product Extended',
+    'summary': "D3 Website Product Extended",
+    'description': """
+        D3 Website Product Extended
+    """,
+    'author': "Dimension3 Technology",
+    'website': "https://d-3system.com.au",
+    'category': 'Website/Website',
+    'version': '18.1',
+    'depends': ['base', 'product', 'website_sale'],
+    'data': [
+        'views/templates.xml',
+        'views/product_views.xml'
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
