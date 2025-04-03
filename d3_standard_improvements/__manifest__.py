1728726060142c0cf414f3d9dbb86e3315dd9292
Juan Arcos <juanparmer@gmail.com>
2025-04-02 19:23:50 -0500

    staging [REF] d3_standard_improvements: change logo and alt on brand

--- d3_standard_improvements/__manifest__.py
+++ d3_standard_improvements/__manifest__.py
@@ -19,12 +19,13 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website/Website',
-    'version': '17.0.2.0.1',
+    'version': '18.0.2.0.1',
 
     # any module necessary for this one to work correctly
     'depends': [
         'mail',
         # 'mrp_account',
+        # 'sale_management',
         'web',
         'website',
         'website_sale'
