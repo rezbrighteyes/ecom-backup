4a5b05eaa074f2bb1c6afbdf55b5f10c557a120b
Jacob Grundy <102121091+D3-JPG@users.noreply.github.com>
2025-05-09 13:44:38 +1000

    Improvements

---
+++ D3_product_image_importer/product_image_importer/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Product Image Importer',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Imports product images from Excel files with URLs or embedded base64',
+    'author': 'Your Company',
+    'depends': ['product'],
+    'data': ['wizard/import_product_images_view.xml'],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
