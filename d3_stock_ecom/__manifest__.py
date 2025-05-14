26cbf30ebf1ac353988de0246ed5e052634b43af
Juan Arcos <juanparmer@gmail.com>
2025-05-14 16:59:55 -0500

    development [REF] d3_stock_ecom: print invoice partner on delivery slip

--- d3_stock_ecom/__manifest__.py
+++ d3_stock_ecom/__manifest__.py
@@ -16,7 +16,7 @@ Modify Delivery Slip report
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Inventory',
-    'version': '18.1',
+    'version': '18.0.1.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['stock'],
--- d3_stock_ecom/models/stock_picking.py
+++ d3_stock_ecom/models/stock_picking.py
@@ -11,8 +11,4 @@ class StockPicking(models.Model):
         if self.sale_id and self.sale_id.partner_invoice_id:
             return True
         else:
-            return (
-                self.move_ids
-                and (self.move_ids[0].partner_id or self.partner_id)
-                and self._is_to_external_location()
-            )
+            super(StockPicking, self).should_print_delivery_address()
