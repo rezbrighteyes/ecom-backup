ed1ee329834721ead99911d9436c16c0a97d0a30
Juan Arcos <juanparmer@gmail.com>
2025-05-19 16:48:12 -0500

    staging [REF] d3_purchase_ecom: refactor aligment issue on purchase report

--- d3_purchase_ecom/__manifest__.py
+++ d3_purchase_ecom/__manifest__.py
@@ -16,7 +16,7 @@ Modify Purchase report, add Total Lines, Total Qty
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Purchase',
-    'version': '18.1',
+    'version': '18.0.1.1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase'],
